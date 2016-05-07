c04724f2a99b3770ef8011b756f60afe48d7ba26
JEFF HENRY <henry_jf@hotmail.com>
2016-05-07 13:05:17 -0400

    added class.js files and started html

---
+++ src/js/classes/jedi.js
@@ -0,0 +1,6 @@
+export class Jedi {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+}
---
+++ src/js/classes/sith.js
@@ -0,0 +1,6 @@
+export class Sith {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+}
