e183d668992af2684bf46e2521804f212c22c036
JEFF HENRY <henry_jf@hotmail.com>
2016-05-07 13:26:12 -0400

    added classes and elements to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 import $ from 'jquery';
 import _ from 'lodash';
+
+import {Game} from './classes/game';
+import {Jedi} from './classes/jedi';
+import {Sith} from './classes/sith';
+
+let jediStatusBar = $('.jediStatus');
+let sithStatusBas = $('.sithStatus');
+let duelButton = $('.lightsaberDuel');
